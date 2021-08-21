dc01b76aafc7892501771332849305261c96f64e
narang09 <khatri.narang09@gmail.com>
2021-08-21 15:02:29 +0530

    mycommit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+
+
+FROM ubuntu
+MAINTAINER khatri.narang09@gmail.com
+RUN apt -y update
+COPY ./run.sh /
+RUN chmod +x /run.sh
+ENTRYPOINT ["/run.sh"]
+CMD ["true" , 2 , 3 ]
---
+++ run.sh
@@ -0,0 +1,18 @@
+
+
+#!/bin/bash
+
+
+while (($1=="true")) ;
+do
+	sleep 1
+	echo "my second & third inpur is $2 $3 "
+	echo "press cltrl+c to stop this loop "
+done
+
+
+
+
+
+
+
